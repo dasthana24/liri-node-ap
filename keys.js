36598a3881b790c53c4047025f8359871d9fddc9
Deepika Asthana <dasthana.24@gmail.com>
2018-05-21 22:02:55 -0600

    initial ommit

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'kogss2uzx0knEnH37hfYHmQTE',
+  consumer_secret: '4DVi3I7nPuY1L6QmdHom5v4y34MhowfWevYz1hAwFEBzjfQmKx',
+  access_token_key: '997223690038317056-tGfR00J1ZtugC4hzCSq4SzZobRDGKw8',
+  access_token_secret: 'DloOaL8Lb8PtL9K44yIamDjw4Xwp8FJx1hk2eRVb2l1cX'
+};
+
+exports.spotify = {
+  id: '6957d2c86aef4fc58525ae570445c459',
+  secret: '50a2249a177141f8996a224734c7fa35'
+};
